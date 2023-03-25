1c2cd83d6cf793d26bbdde1eb0abc67f9e5e5418
alaska <rus.rakhmatullin.06@bk.ru>
2023-03-25 16:04:30 +0300

    323

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 function Click(){
     alert("Функция не доступна, сайт в  разработке!")
 }
+
+var  body = document.getElementById('body')
+
+function Check(){
+    var checkBox = document.getElementById("check")
+    if (checkBox.checked == true){
+        body.style.backgroundColor = '#2e2e2ed8'
+        
+   }else{
+        body.style.backgroundColor = ''
+   }
+}
