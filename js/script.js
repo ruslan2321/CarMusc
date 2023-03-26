5fb591a40d48a231ac111819f72cbd662231852f
alaska <rus.rakhmatullin.06@bk.ru>
2023-03-26 11:33:26 +0300

    6

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function Click(){
-    var pup = document.getElementById('pup')
-    pup.classList.toggle('show')
-}
-function profile(){
-    var pup_profile = document.getElementById('pop-profile')
-    pup_profile.classList.toggle('show-profile')
-}
